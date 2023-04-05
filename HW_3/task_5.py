6d081448ac8bdcc3015f08074df4dd9401186964
Lera Roy <valeria98.val@gmail.com>
2023-04-05 15:20:44 +0300

    update HW 3

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -14,8 +14,8 @@
 
 rows = int(input("Enter n: "))
 for i in range(1, rows + 1):
-    for j in range(i, rows + 1):
-        print(" ", end =" ")
+    for j in range(i, rows):
+        print(" ", end=" ")
     for j in range(1, i + 1):
         print(j, end=" ")
     for j in range(i - 1, 0, -1):
